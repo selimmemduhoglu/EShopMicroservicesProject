2df52755ca76ffbb5dbdf4856e5461fbdb306c49
MuhammedSelimMemduho <MuhammedSelimMemduho@DESKTOP-0VH77KH>
2024-08-16 15:45:45 +0300

    added : CQRS structure

--- eshop-microservices-project/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ eshop-microservices-project/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,5 +1,13 @@
-﻿namespace Catalog.API.Products.CreateProduct;
+﻿using MediatR;
 
-public class CreateProductHandler
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : IRequest<CreateProductResult>;
+public record CreateProductResult(Guid Id);
+internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
 {
+    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
 }
